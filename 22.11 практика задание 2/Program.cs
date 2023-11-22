743fe1c25bdc7fed6d20c7474303365e90e5b988
MSI <MSI@MSI>
2023-11-22 15:40:52 +0300

    Добавьте файлы проекта.

---
+++ 22.11 практика задание 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11_практика_задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Ведите фамилию: ");
+            string name2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"имя: {name1}, фамилия: {name2}");
+            Console.ReadKey();
+        }
+    }
+}
